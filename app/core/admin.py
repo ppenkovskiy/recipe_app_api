8bd649c23bcfa889230b42d581e85d8de28fb9b1
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-11-20 17:02:02 +0300

    finished_step_56

---
+++ app/core/admin.py
@@ -0,0 +1,16 @@
+"""
+Django admin customization.
+"""
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+# register User model with UserAdmin class
+admin.site.register(models.User, UserAdmin)
+
