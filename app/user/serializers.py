6fc0851c235e5ad37acf02b145082ea3fe025e05
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-11-23 19:31:57 +0300

    finished_step_68

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -2,10 +2,11 @@
 Serializers for the user API view.
 """
 from django.contrib.auth import get_user_model
-# import serialize module from rest framework
+# Serializers module includes different tools that we need for defining serialize.
 from rest_framework import serializers
 
-# serealize is simply just a way to convert objects to and from python objects
+
+# Serialization is simply just a way to convert objects to and from Python objects.
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
 
@@ -15,6 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['email', 'password', 'name']
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
+    # This method will be called when tha validation is successful.
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
