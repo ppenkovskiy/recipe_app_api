a7f7d9abe03314b0ca628160a1f6f8c80589eee3
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-12-03 13:51:50 +0300

    start_86

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -17,7 +17,6 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
-
     serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
