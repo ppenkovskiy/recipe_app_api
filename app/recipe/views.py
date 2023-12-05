8f25f8c88a5c7e55a7834e9007daf80fb8a14e90
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-12-05 12:14:12 +0300

    fin_93

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,15 +2,19 @@
 Views for the recipe APIs.
 """
 # Importing the base class ModelViewSet for creating a view that handles CRUD operations on a model.
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins,
+)
 # Importing token-based authentication for the API.
 from rest_framework.authentication import TokenAuthentication
 # Importing a permission class to ensure that only authenticated users can access the view.
 from rest_framework.permissions import IsAuthenticated
-# Importing the Recipe model from the core app. This assumes that there's a
-# model named Recipe in the core app, and this view is designed to work with it.
-from core.models import Recipe
-# Importing the serializer class (RecipeSerializer) from the recipe app,
+from core.models import (
+    Recipe,
+    Tag,
+)
+# Importing serializer class (RecipeSerializer) from the recipe app,
 # which is used to serialize/deserialize Recipe objects.
 from recipe import serializers
 
@@ -37,3 +41,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
